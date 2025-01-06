f6e340b227d2708a16aded451e0f3956bc4ecc27
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-01-06 22:04:58 +0800

    优化

--- app/(tabs)/users.tsx
+++ app/(tabs)/users.tsx
@@ -1,13 +1,14 @@
 import { View, Text, Button } from "react-native";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 
 export default function Index() {
+   const openUser = () => {
+          router.navigate("/users/profile");
+      }
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>这里是用户首页</Text>
-      <Link href="/users/profile" asChild>
-        <Button title="跳转到用户信息页" />
-      </Link>
+      <Button title="跳转到用户信息页" onPress={openUser} />
     </View>
   );
 }
