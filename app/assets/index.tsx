2a7229bd914a44646322edc6098e51e3cb71eb7b
wu-yiqiang <wu_yiqiang@outlook.com>
2025-01-15 01:35:02 +0800

    添加动态数据

--- app/assets/index.tsx
+++ app/assets/index.tsx
@@ -14,9 +14,9 @@ export default function Assets(props: any) {
   };
   const onScroll = (event: Event) => {
     const { contentOffset, contentSize, layoutMeasurement } = event.nativeEvent;
-    const isNearTop = contentOffset.y <= 30; // 10是阈值，可以根据需要调整
+    const isNearTop = contentOffset.y <= 50; // 10是阈值，可以根据需要调整
     const isNearBottom =
-      contentOffset.y + layoutMeasurement.height >= contentSize.height - 30; // 10是阈值，可以根据需要调整
+      contentOffset.y + layoutMeasurement.height >= contentSize.height - 50; // 10是阈值，可以根据需要调整
 
     if (isNearTop && refreshing === false) {
       console.log("滚动顶部了");
@@ -24,8 +24,8 @@ export default function Assets(props: any) {
 
     if (isNearBottom) {
       const item = {
-        id: 1,
-        title: new Date().getTime(),
+        id: new Date().getTime(),
+        title: "音响",
         categopry: "办公用品",
         department: "人事行政部",
         number: "FZC00124533",
@@ -38,34 +38,80 @@ export default function Assets(props: any) {
       setItems([...items, item]);
       console.log("滚动底部了");
     }
-
-     const init = async () => {
-       const res = await mockRequest();
-       setItems(res);
-       return res;
-     };
-    useEffect(() => {
-      init();
-    }, [count]);
-  };
- 
-    const mockRequest = () => {
-    let data = []
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          data.push({
-            id: i.toString(),
-          })
-        }
-        resolve(data)
-      }, 1000)
-    })
   }
   const init = async () => {
-    
-    console.log(res);
+    setItems([{
+        id: 1,
+        title: "椅子",
+        categopry: "办公用品",
+        department: "人事行政部",
+        number: "FZC00124534",
+        status: 0,
+        assetsStatus: 1,
+        belong: "sutter",
+        updator: "Tom",
+        update: "2025-11-23 12:34",
+      },{
+        id: 2,
+        title: "计算器",
+        categopry: "办公用品",
+        department: "人事行政部",
+        number: "FZC00124543",
+        status: 1,
+        assetsStatus: 1,
+        belong: "sutter",
+        updator: "Tom",
+        update: "2025-11-23 12:34",
+      },{
+        id: 9,
+        title: "会议桌",
+        categopry: "办公用品",
+        department: "人事行政部",
+        number: "FZC00124533",
+        status: 0,
+        assetsStatus: 1,
+        belong: "sutter",
+        updator: "Tom",
+        update: "2025-11-23 12:34",
+      },{
+        id: 3,
+        title: "苹果笔记本",
+        categopry: "电脑",
+        department: "开发部",
+        number: "FZC00124533",
+        status: 0,
+        assetsStatus: 1,
+        belong: "sutter",
+        updator: "Tom",
+        update: "2025-11-23 12:34",
+      },{
+        id: 4,
+        title: new Date().getTime(),
+        categopry: "服务器",
+        department: "系统一部",
+        number: "FZC00156567",
+        status: 0,
+        assetsStatus: 0,
+        belong: "sutter",
+        updator: "Tom",
+        update: "2025-11-23 12:34",
+      },{
+        id: 5,
+        title: new Date().getTime(),
+        categopry: "台式机",
+        department: "开发部",
+        number: "FZC00124553",
+        status: 1,
+        assetsStatus: 1,
+        belong: "sutter",
+        updator: "Tom",
+        update: "2025-11-23 12:34",
+      }])
   }
+  useEffect(() => {
+    init()
+  }, [])
+  if (!items.length) return <Text>暂无数据</Text>
   return (
     <View style={styles.Assets}>
       <ScrollView
