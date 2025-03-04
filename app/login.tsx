7a73131e8fb80f98085485bc1944a3175ad727ee
wu-yiqiang <wu_yiqiang@outlook.com>
2025-03-05 01:22:40 +0800

    11

--- app/login.tsx
+++ app/login.tsx
@@ -1,21 +1,21 @@
 import { View, Text, StyleSheet, Image, TextInput, Button, SafeAreaView } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Logo from '@/assets/images/logo.png'
-// import { TextInput } from 'react-native-paper'
 import { Link, router, Stack } from 'expo-router'
 import { PostLogin } from '@/api/public'
 import { useState } from 'react'
 
 export default function Login() {
   const [formState, setFormState] = useState({ username: 'python222', password: '123456' })
-  const handleToHome = async () => {
+  const handleLogin = async () => {
     const { data } = await PostLogin(formState)
     const token = data?.token
     if (token) {
       await AsyncStorage.setItem('msAppToken', token)
-      router.navigate({ pathname: '/' })
+      // router.navigate({ pathname: '/' })
     }
   }
+  const handleRegister = () => {}
   return (
     <View style={styles.Login}>
       <Stack.Screen
@@ -33,7 +33,14 @@ export default function Login() {
           <TextInput style={styles.input} placeholder="邮箱" value={formState.username} onChangeText={(value) => setFormState({ username: value, password: formState.password })} />
           <TextInput style={styles.input} inlineImageLeft="search_icon" value={formState.password} placeholder="密码" secureTextEntry={true} onChangeText={(value) => setFormState({ username: formState.username, password: value })} />
         </View>
-        <Button title="登陆" onPress={handleToHome} />
+        <View style={styles.toolBox}>
+          <View style={styles.button}>
+            <Button title="注册" color={'#d5d5d5'} onPress={handleRegister} />
+          </View>
+          <View style={styles.button}>
+            <Button title="登陆" onPress={handleLogin} />
+          </View>
+        </View>
       </View>
     </View>
   )
@@ -79,6 +86,15 @@ const styles = StyleSheet.create({
     width: '100%',
     fontSize: 14,
     height: 40,
-    padding: 10,
+    padding: 10
+  },
+  toolBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    // flex: 1,
+    width: 80
   }
 })
